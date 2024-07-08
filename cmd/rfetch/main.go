3b49fd20b7fd605797ea8df5c97adaaf45119e92
TWolfis <Thomas.wolfis@gmail.com>
2024-07-08 10:04:14 +0200

    modified rfetch

--- cmd/rfetch/main.go
+++ cmd/rfetch/main.go
@@ -4,22 +4,39 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand/v2"
 	"os"
 
 	"github.com/TWolfis/ietfrfc"
 )
 
 var (
-	n int
+	number int
+	short  bool
 )
 
 func main() {
-	flag.IntVar(&n, "num", 1, "rfc number to fetch")
+	// generate randum number
+	rnRfc := rand.IntN(10000)
+	flag.IntVar(&number, "num", rnRfc, "rfc number to fetch")
+	flag.BoolVar(&short, "short", false, "short output")
+
+	// parse flags
 	flag.Parse()
 
-	rfc, err := ietfrfc.Get(n)
+	rfc, err := ietfrfc.Get(number)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	if short {
+		fmt.Println(rfc.Title)
+		fmt.Println("RFC", rfc.Number)
+		fmt.Println(rfc.Abstract)
+		fmt.Println(rfc.Author)
+		fmt.Println(rfc.URL)
+
 		os.Exit(0)
 	}
 
